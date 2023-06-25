19f77b9757c0fadf9d7b4dd3616366cba111c121
CIKADA <adel.garipovazn@mail.ru>
2023-06-26 01:02:54 +0300

    Refactor kebab-btn listener

--- src/js/kebab-btn.js
+++ src/js/kebab-btn.js
@@ -1,42 +1,32 @@
 function addKebabBtnListener() {
-  // обрабатываем нажатие на kebab-btn
-  const kebabButtons = document.querySelectorAll(".kebab-btn");
-  kebabButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      button.classList.toggle("kebab-btn_active");
-      const kebabDropdownMenu = button.nextElementSibling;
-      kebabDropdownMenu.classList.toggle("kebab-btn__dropdown-menu_active");
-    });
-  });
-
-  // скрываем выпадающий список при щелчке в любом месте, кроме кнопки и списка // ToDo
   document.addEventListener("click", (event) => {
-    const kebabButtons = document.querySelectorAll(".kebab-btn");
-    const kebabDropdownMenus = document.querySelectorAll(
-      ".kebab-btn__dropdown-menu"
-    );
     const target = event.target;
+    const isKebabButton = target.closest(".kebab-btn");
+    const isDropdownMenu = target.closest(".kebab-btn__dropdown-menu");
 
-    let isButtonClick = false;
-    let isDropdownMenuClick = false;
+    if (!isKebabButton && !isDropdownMenu) {
+      const activeDropdownMenus = document.querySelectorAll(
+        ".kebab-btn__dropdown-menu_active"
+      );
+      const activeKebabButtons = document.querySelectorAll(".kebab-btn_active");
 
-    kebabButtons.forEach((button) => {
-      if (button.contains(target)) {
-        isButtonClick = true;
-      }
-    });
-
-    kebabDropdownMenus.forEach((menu) => {
-      if (menu.contains(target)) {
-        isDropdownMenuClick = true;
-      }
-    });
-
-    if (!isButtonClick && !isDropdownMenuClick) {
-      kebabDropdownMenus.forEach((menu) => {
+      activeDropdownMenus.forEach((menu) => {
         menu.classList.remove("kebab-btn__dropdown-menu_active");
-        const button = menu.previousElementSibling;
+      });
+
+      activeKebabButtons.forEach((button) => {
         button.classList.remove("kebab-btn_active");
+      });
+
+      const activeTaskItems = document.querySelectorAll(".task-item_active");
+      const activeProjectItems = document.querySelectorAll(
+        ".project-item_active"
+      );
+      activeTaskItems.forEach((taskItem) => {
+        taskItem.classList.remove("task-item_active");
+      });
+      activeProjectItems.forEach((taskItem) => {
+        taskItem.classList.remove("project-item_active");
       });
     }
   });
