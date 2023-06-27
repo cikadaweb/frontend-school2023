b85a9b51d813c1e30b600446f29e7651866eab2e
CIKADA <adel.garipovazn@mail.ru>
2023-06-27 23:11:24 +0300

    Init vue-cli project

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TheHeader />
+    <main class="main">
+       <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/header/TheHeader.vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    TheHeader
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/layouts/DefaultLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'HomePage',
+        component: () => import('@/views/HomePage.vue')
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
