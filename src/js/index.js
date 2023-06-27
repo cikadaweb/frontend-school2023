80067795c563f156d05622ea4f6d13c79036f136
CIKADA <adel.garipovazn@mail.ru>
2023-06-27 15:15:59 +0300

    Add active link handler

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,26 @@
 import {
   createNavigation,
   addNavigationListener,
-} from "../components/navigation/index";
+} from "@/components/navigation/index";
 import {
   createProjectItems,
   addProjectItemListener,
-} from "../components/project-item/index";
+} from "@/components/project-item/index";
 import {
   createTaskItems,
   addTaskItemListener,
-} from "../components/task-item/index";
-import { createPlaceholder } from "../components/placeholder/index";
-import { addKebabBtnListener } from "./kebab-btn";
+} from "@/components/task-item/index";
+import { createPlaceholder } from "@/components/placeholder/index";
+import { addKebabBtnListener } from "@/js/kebab-btn.js";
 
-import data from "../js/data";
+import data from "@/js/data";
 
-import "../scss/style.scss";
-import "../js/register";
+import "@/scss/style.scss";
+import "@/js/register";
 
 function initApp() {
   let app = document.querySelector(".app");
+  const activeLink = localStorage.getItem("active-link");
 
   let navHtml = createNavigation();
   let projectItemHtml = createProjectItems(data.projects);
@@ -35,6 +36,25 @@ function initApp() {
   addKebabBtnListener();
   addTaskItemListener();
   addProjectItemListener();
+
+  setActiveLinkClass();
+}
+function setActiveLinkClass() {
+  const currentActiveLink = localStorage.getItem("active-link");
+  if (currentActiveLink === "tasks") {
+    document
+      .querySelector(".header__link[data-items=tasks]")
+      .classList.add("header__link_active");
+    return;
+  } else if (currentActiveLink === "users") {
+    document
+      .querySelector(".header__link[data-items=users]")
+      .classList.add("header__link_active");
+    return;
+  }
+  document
+    .querySelector(".header__link[data-items=projects]")
+    .classList.add("header__link_active");
 }
 
 initApp();
