2b3b815ed17a9ab4091e7817013e427b3a54014f
CIKADA <adel.garipovazn@mail.ru>
2023-06-29 00:19:48 +0300

    Add global style.scss, svg-sprite config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "~@/assets/scss/style.scss";' // подключение style.scss для всего проекта
+      }
+    }
+  },
+  pluginOptions: {
+    svgSprite: {
+      dir: './src/assets/icons',
+      test: /\.svg$/,
+      loaderOptions: {
+        extract: false
+      }
+    }
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('svg-sprite')
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+  }
 })
