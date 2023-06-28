39af5d7c1ab9d2d2d03ec87d6f033bb10d14ca11
CIKADA <adel.garipovazn@mail.ru>
2023-06-29 00:24:02 +0300

    Create SVGicon component

---
+++ src/components/svg-icon/SVGIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg :width="width" :height="height">
+    <use :xlink:href="id"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'SVGIcon',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      required: true
+    },
+    height: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
