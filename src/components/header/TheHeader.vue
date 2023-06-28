37d374091f0af4f50f77a02374eff7db707723a1
CIKADA <adel.garipovazn@mail.ru>
2023-06-28 21:12:26 +0300

    Delete import styles

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -72,9 +72,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/variables';
-@import '@/assets/scss/mixins';
-@import '@/assets/scss/extensions';
 .header {
   padding: 24px 0;
   @extend %bg-primary-default;
