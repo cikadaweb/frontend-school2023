862638797cd821bced94c1b70769609423d59650
CIKADA <adel.garipovazn@mail.ru>
2023-06-30 01:02:29 +0300

    Create user store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import user from '@/store/user'
+import project from '@/store/project'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: { user, project }
 })
---
+++ src/store/user.js
@@ -0,0 +1,8 @@
+export default {
+  state: {
+    currentUser: {}
+  },
+  mutations: {},
+  actions: {},
+  getters: {}
+}
