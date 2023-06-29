5881e64d05c2b21be3d1982d06351451ee0a33ed
CIKADA <adel.garipovazn@mail.ru>
2023-06-30 01:00:28 +0300

    Add utils

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/helpers/register-assets'
+import '@/utils/register-assets'
+import '@/utils/register-components'
 
 Vue.config.productionTip = false
 
