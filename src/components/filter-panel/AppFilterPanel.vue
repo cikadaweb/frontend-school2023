52560cd5e3334577b33cbdec1d93dc32dbae03e2
CIKADA <adel.garipovazn@mail.ru>
2023-07-03 13:04:06 +0300

    Create FilterPanel

---
+++ src/components/filter-panel/AppFilterPanel.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="filter-panel" @submit.prevent="submitHandler">
+    <AppInput
+      name="searchValue"
+      type="search"
+      placeholder="Текст..."
+      width="100%"
+      v-model="formData.searchField"
+      :state="searchFieldClass"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AppFilterPanel',
+  data () {
+    return {
+      formData: {
+        searchField: ''
+      }
+    }
+  },
+  computed: {
+    searchFieldClass () {
+      if (this.formData.searchField.length === 0) {
+        return 'empty'
+      }
+      if (this.formData.searchField.length > 0 && this.formData.searchField.length < 5) {
+        return 'error'
+      }
+      return 'default'
+    }
+  }
+}
+</script>
