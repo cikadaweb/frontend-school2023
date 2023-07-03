4980d6d824e60535b00d91106c2211db6781110f
CIKADA <adel.garipovazn@mail.ru>
2023-07-03 11:53:05 +0300

    Create Input component

---
+++ src/components/ui-elements/input/AppInput.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="input" :style="{ width: width }">
+    <label :for="name" class="input__label">
+      <input
+        :class="[
+          'input__text',
+          `input_${state}`,
+        ]"
+        :type="type"
+        :name="name"
+        :id="name"
+        :placeholder="placeholder"
+        :value="value"
+        :disabled="disabled"
+        @input="updateValue($event)"
+        >
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppInput',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      required: false
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      default: '300px'
+    },
+    state: {
+      type: String,
+      default: 'default'
+    }
+  },
+  emits: ['input'],
+  methods: {
+    updateValue (event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.input__text {
+  @include input-reset;
+  outline: none;
+}
+.input__text {
+  padding: 10px 12px;
+  border-radius: $btn-radius;
+}
+
+.input_default {
+  background: $bg-secondary;
+  color: $font-primary-default;
+  border: 1px solid $border-default;
+
+  // &:hover {
+  //   background: $bg-secondary;
+  //   color: $font-placeholder;
+  //   border: 1px solid $border-hover;
+  // }
+  &:active {
+    background: $bg-secondary;
+    color: $font-primary-default;
+    border: 1px solid $border-active;
+  }
+  &:disabled {
+    background: $bg-disabled;
+    color: $font-disabled;
+    border: 1px solid $border-disabled;
+  }
+}
+.input_error {
+  background: $bg-secondary;
+  color: $font-primary-default;
+  border: 1px solid $border-error;
+}
+</style>
