880c62fa493c63af0894aa73d2747ddd5408f9e0
CIKADA <adel.garipovazn@mail.ru>
2023-07-03 11:51:33 +0300

    Change styles

--- src/components/project/AppProjectItem.vue
+++ src/components/project/AppProjectItem.vue
@@ -52,7 +52,6 @@
 <script>
 
 export default {
-  components: {},
   name: 'AppProjectItem',
   props: {
     item: {
@@ -192,7 +191,7 @@ export default {
   padding: 8px 0;
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
   top: 54px;
-  right: 16px;
+  right: 0;
 }
 
 .kebab-btn__dropdown-menu_active {
