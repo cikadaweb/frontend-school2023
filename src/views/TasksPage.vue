321b17f9a21089317b891d2f0a131502e0971374
CIKADA <adel.garipovazn@mail.ru>
2023-07-03 11:52:28 +0300

    Create Tasks page

--- src/views/TasksPage.vue
+++ src/views/TasksPage.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="tasks-page">
-    <h1>TasksPage</h1>
+  <div class="task-page">
+    <div class="container">
+      <div class="task-page__row">
+        <div class="task-page__column">
+          <AppFilterPanel />
+        </div>
+        <div class="task-page__column">
+          <div v-if="tasks.length !== 0">
+            <AppTaskItem v-for="(task, idx) in tasks" :key="idx" :item="task" />
+          </div>
+
+          <AppPlaceholder v-else>
+            <div>Не создана ни одна задача</div>
+          </AppPlaceholder>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import AppTaskItem from '@/components/task/AppTaskItem.vue'
+import AppFilterPanel from '@/components/filter-panel/AppFilterPanel.vue'
 
 export default {
-  name: 'TasksPage'
+  name: 'TasksPage',
+  components: {
+    AppTaskItem, AppFilterPanel
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    clickOnButton () {
+      console.log('clickOnButton')
+    }
+  },
+  computed: {
+    tasks () {
+      return this.$store.state.task.tasks
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.task-page__row {
+  display: flex;
+  flex-direction: column;
+}
+
+.task-page__column {
+  margin-top: 24px;
+}
+
+.task-page__placeholder {
+  margin-top: 10px;
+}
+</style>
