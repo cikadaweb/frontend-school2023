57c8bca8008fee744920bce19ba8574e72920073
CIKADA <adel.garipovazn@mail.ru>
2023-07-09 01:42:57 +0300

    Refactor Input

--- src/components/ui-elements/input/AppInput.vue
+++ src/components/ui-elements/input/AppInput.vue
@@ -1,26 +1,12 @@
 <template>
-  <div class="input" :style="{ width: width }">
+  <div class="input" :style="{ width: width }" @click="clickOnIcon($event)">
     <label :for="name" class="input__label">
-      <input
-        :class="[
-          'input__text',
-          `input_${state}`,
-        ]"
-        :type="type"
-        :name="name"
-        :id="name"
-        :placeholder="placeholder"
-        :value="value"
-        :disabled="disabled"
-        @input="updateValue($event)"
-      >
-      <AppIcon
-          v-if="icon"
-          class="input__icon"
-          :id="`#${icon}`"
-          width="24"
-          height="24"
-      />
+      <input :class="[
+        'input__text',
+        `input_${state}`,
+      ]" :type="type" :name="name" :id="name" :placeholder="placeholder" :value="value" :disabled="disabled"
+        @input="updateValue($event)">
+      <AppIcon v-if="icon" class="input__icon" :id="`#${icon}`" width="24" height="24" />
     </label>
   </div>
 </template>
@@ -47,7 +33,7 @@ export default {
     },
     placeholder: {
       type: String,
-      required: true
+      required: false
     },
     width: {
       type: String,
@@ -60,12 +46,23 @@ export default {
     icon: {
       type: String,
       required: false
+    },
+    label: {
+      type: String,
+      required: false
+    },
+    required: {
+      type: Boolean,
+      default: false
     }
   },
   emits: ['input'],
   methods: {
     updateValue (event) {
       this.$emit('input', event.target.value)
+    },
+    clickOnIcon (event) {
+      this.$emit('click-on-icon', event)
     }
   }
 }
@@ -73,22 +70,28 @@ export default {
 
 <style lang="scss">
 .input__label {
+  width: 100%;
   position: relative;
   display: inline-block;
 }
+
 .input__text {
   @include input-reset;
   outline: none;
   transition: all 0.3s ease 0s;
 }
+
 .input__text {
+  width: 100%;
   padding: 10px 45px 10px 12px;
   border-radius: $btn-radius;
 }
+
 .input__icon {
   position: absolute;
   top: 9px;
   right: 9px;
+  cursor: pointer;
 }
 
 .input_default {
@@ -101,6 +104,7 @@ export default {
     color: $font-primary-default;
     border: 1px solid $border-active;
   }
+
   &:disabled {
     background: $bg-disabled;
     color: $font-disabled;
@@ -112,6 +116,7 @@ export default {
   background: $bg-secondary;
   color: $font-placeholder;
   border: 1px solid $border-default;
+
   &:hover {
     background: $bg-secondary;
     color: $font-placeholder;
