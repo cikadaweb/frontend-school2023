441762f82a194bd4996d8379f8bbd73a6cd393d2
CIKADA <adel.garipovazn@mail.ru>
2023-07-09 01:42:22 +0300

    Create InputLabel

---
+++ src/components/ui-elements/input/AppInputLabel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="label">
+    <slot />
+    <span v-if="required" class="label__require">*</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    required: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.label {
+  @include font(14px, 400, normal);
+  color: $font-disabled;
+  min-width: 120px;
+}
+
+.label__require {
+  color: $font-error;
+  margin-left: 4px;
+}
+</style>
