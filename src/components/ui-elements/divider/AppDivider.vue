69460f147eac75c500a4ff2fa9e5f8bc20cfac6c
CIKADA <adel.garipovazn@mail.ru>
2023-07-09 01:40:43 +0300

    Create Divider

---
+++ src/components/ui-elements/divider/AppDivider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="divider"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.divider {
+  width: 100%;
+  height: 1px;
+  background-color: $border-default;
+}
+</style>
