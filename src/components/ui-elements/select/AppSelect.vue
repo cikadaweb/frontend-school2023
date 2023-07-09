9ef7732970935ab7c858d2642cef5c3549011d8f
CIKADA <adel.garipovazn@mail.ru>
2023-07-09 23:32:15 +0300

    Add Select styles

--- src/components/ui-elements/select/AppSelect.vue
+++ src/components/ui-elements/select/AppSelect.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="select" :style="{ width: width }" ref="select">
-    <div class="select__field" @click="toggleOptionsVisible">{{ selected }}
+    <div
+      class="select__field"
+      :class="[disabled ? 'select__field_disabled' : '', isShowOptions ? 'select__field_active' : '']"
+      @click="toggleOptionsVisible"
+    >{{ selected }}
       <AppIcon
-        class="select__icon"
+        v-if="clearIcon && isOptionChosen"
+        class="select__icon select__icon_clear"
+        id="#clear"
+        width="24"
+        height="24"
+        @click-on-icon="clearSelect"
+        ref="clearSelectBtn"
+      />
+      <AppIcon
+        class="select__icon select__icon_down"
+        :class="isShowOptions ? 'select__icon_active' : ''"
         id="#drop-down"
         width="24"
         height="24"
       />
     </div>
-    <div v-if="isShowOptions" class="select__field-list" :style="{ width: width }">
-      <span class="select__option" v-for="option in options" :key="option.value" @click="selectOption(option)">{{ option.name }}</span>
-    </div>
+    <ul
+      v-if="isShowOptions"
+      class="select__list"
+      :style="{ width: width }"
+    >
+      <li
+        class="select__item"
+        :class="option.name === selected ? 'select__item_active' : 'asdas'"
+        v-for="option in options"
+        :key="option.value"
+        @click="selectOption(option)"
+        >{{ option.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -31,6 +56,14 @@ export default {
     width: {
       type: String,
       default: '300px'
+    },
+    disabled: {
+      type: Boolean,
+      required: false
+    },
+    clearIcon: {
+      type: Boolean,
+      default: false
     }
   },
   emits: ['select-option'],
@@ -46,12 +79,17 @@ export default {
     }
   },
   methods: {
-    toggleOptionsVisible () {
-      this.isShowOptions = !this.isShowOptions
+    toggleOptionsVisible (event) {
+      if (!this.disabled && !event.target.classList.contains('select__icon_clear')) {
+        this.isShowOptions = !this.isShowOptions
+      }
     },
     selectOption (option) {
       this.isShowOptions = false
       this.$emit('select-option', option)
+    },
+    clearSelect () {
+      this.$emit('clear-select')
     },
     handleOutsideClick (event) {
       const select = this.$refs.select
@@ -61,6 +99,14 @@ export default {
       ) {
         this.isShowOptions = false
       }
+    }
+  },
+  computed: {
+    isOptionChosen () {
+      if (!this.options.some(option => option.name === this.selected)) {
+        return false
+      }
+      return true
     }
   }
 }
@@ -76,19 +122,39 @@ export default {
   border: 1px solid $border-default;
   padding: 10px 45px 10px 12px;
   border-radius: $btn-radius;
+
+  &_active {
+    border: 1px solid $border-active;
+  }
+
+  &_disabled {
+    background: $bg-disabled;
+    color: $font-disabled;
+    border: 1px solid $border-disabled;
+  }
 }
 .select__icon {
   position: absolute;
+  color: $font-disabled;
+  cursor: pointer;
+  transition: all 0.3s ease 0s;
+}
+.select__icon_clear {
+  top: 9px;
+  right: 41px;
+}
+.select__icon_down {
   top: 9px;
   right: 8px;
 }
 .select__icon_active {
   transform: rotate(180deg);
 }
-.select__field-list {
+.select__list {
   position: absolute;
   top: 50px;
   left: 0;
+  z-index: 1;
 
   display: flex;
   flex-direction: column;
@@ -96,13 +162,18 @@ export default {
   box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25);
   padding: 8px 0;
 }
-.select__option {
+.select__item {
   @include font(14px, 400, 19px);
   color: $font-primary-default;
   padding: 8px 16px;
   white-space: nowrap;
   cursor: pointer;
   transition: all 0.2s ease 0s;
+
+  &_active {
+    background-color: $font-secondary-active;
+  }
+
   &:hover {
     background-color: $font-secondary-active;
   }
