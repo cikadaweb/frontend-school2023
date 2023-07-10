f76196518eac6833b5f5fb55b72adb0e2efa670e
CIKADA <adel.garipovazn@mail.ru>
2023-07-10 22:20:44 +0300

    Add AppInput clearIcon

--- src/components/ui-elements/input/AppInput.vue
+++ src/components/ui-elements/input/AppInput.vue
@@ -1,12 +1,42 @@
 <template>
-  <div class="input" :style="{ width: width }" @click="clickOnIcon($event)">
-    <label :for="name" class="input__label">
-      <input :class="[
-        'input__text',
-        `input_${state}`,
-      ]" :type="type" :name="name" :id="name" :placeholder="placeholder" :value="value" :disabled="disabled"
-        @input="updateValue($event)">
-      <AppIcon v-if="icon" class="input__icon" :id="`#${icon}`" width="24" height="24" />
+  <div
+    class="input"
+    :style="{ width: width }"
+    @click="clickOnIcon($event)"
+  >
+    <label
+      :for="name"
+      class="input__label">
+      <input
+        :class="[
+          'input__text',
+          `input_${state}`,
+          `input__text_${iconPadding}`
+          ]"
+        :type="type"
+        :name="name"
+        :id="name"
+        :placeholder="placeholder"
+        :value="value"
+        :disabled="disabled"
+        @input="updateValue($event)"
+      >
+      <AppIcon
+        v-if="clearIcon && isInputEmpty"
+        class="input__icon input__icon_clear"
+        id="#clear"
+        width="16"
+        height="16"
+        @click-on-icon="clearInput"
+      />
+      <AppIcon
+        v-if="icon"
+        class="input__icon input__icon_search"
+        :id="`#${icon}`"
+        width="24"
+        height="24"
+        @click-on-icon="searchResults"
+      />
     </label>
   </div>
 </template>
@@ -54,6 +84,10 @@ export default {
     required: {
       type: Boolean,
       default: false
+    },
+    clearIcon: {
+      type: Boolean,
+      required: false
     }
   },
   emits: ['input'],
@@ -63,6 +97,29 @@ export default {
     },
     clickOnIcon (event) {
       this.$emit('click-on-icon', event)
+    },
+    clearInput () {
+      this.$emit('clear-input')
+    },
+    searchResults () {
+      this.$emit('search-results')
+    }
+  },
+  computed: {
+    isInputEmpty () {
+      if (this.value === '') {
+        return false
+      }
+      return true
+    },
+    iconPadding () {
+      if (this.icon) {
+        if (this.clearIcon) {
+          return 'pr-2'
+        }
+        return 'pr-1'
+      }
+      return 'pr'
     }
   }
 }
@@ -81,17 +138,33 @@ export default {
 
 .input__text {
   width: 100%;
-  padding: 10px 12px;
   border-radius: $btn-radius;
   outline: none;
   transition: all 0.3s ease 0s;
+  &_pr {
+    padding: 10px 12px;
+  }
+  &_pr-1 {
+    padding: 10px 41px 10px 12px;
+  }
+  &_pr-2 {
+    padding: 10px 65px 10px 12px;
+  }
 }
 
 .input__icon {
   position: absolute;
+  cursor: pointer;
+}
+
+.input__icon_clear {
+  top: 12px;
+  right: 41px;
+}
+
+.input__icon_search {
   top: 9px;
   right: 9px;
-  cursor: pointer;
 }
 
 .input_default {
