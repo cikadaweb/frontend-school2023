f04bc18a5e66f28cbc336d5f8cf8f3e6e277da96
CIKADA <adel.garipovazn@mail.ru>
2023-07-10 13:03:31 +0300

    Create User badge

---
+++ src/components/ui-elements/badges/AppUserBadge.vue
@@ -0,0 +1,69 @@
+<template>
+  <span :class="[
+    'badge',
+    `badge_${badgeStatus}`,
+  ]">
+    {{ badgeName }}
+  </span>
+</template>
+
+<script>
+
+export default {
+  name: 'AppUserBadge',
+  props: {
+    state: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    badgeStatus () {
+      const state = this.state.toLowerCase()
+      switch (state) {
+        case 'active':
+          return 'primary'
+        case 'blocked':
+          return 'disabled'
+        case 'deleted':
+          return 'deleted'
+        default:
+          return 'primary'
+      }
+    },
+    badgeName () {
+      const state = this.state.toLowerCase()
+      switch (state) {
+        case 'active':
+          return 'Активен'
+        case 'blocked':
+          return 'Заблокирован'
+        case 'deleted':
+          return 'Удален'
+        default:
+          return 'primary'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.badge {
+  padding: 0 8px;
+  border-radius: $btn-radius;
+  color: $font-secondary;
+}
+.badge_primary {
+  background-color: $bg-primary-default;
+}
+.badge_bronze {
+  background-color: $bg-bronze;
+}
+.badge_deleted {
+  background-color: $font-error;
+}
+.badge_disabled {
+  background-color: $font-disabled;
+}
+</style>
