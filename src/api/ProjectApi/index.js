013ece1ed9538971fe308aa4be65763916065770
CIKADA <adel.garipovazn@mail.ru>
2023-07-16 21:19:23 +0300

    Change ProjectApi

--- src/api/ProjectApi/index.js
+++ src/api/ProjectApi/index.js
@@ -1,9 +1,23 @@
-import { DefaultAPIInstance } from '@/api/index'
+import { DefaultAPIInstance } from '@/api/index';
 
 export const ProjectsApi = {
-  getProjects (name, code) {
-    const url = '/projects'
-    const data = { name, code }
-    return DefaultAPIInstance.post(url, data)
+  getProjects(objData) {
+    const url = '/projects/search';
+    const data = objData;
+    return DefaultAPIInstance.post(url, data);
+  },
+  createProject(name, code) {
+    const url = '/projects';
+    const data = { name, code };
+    return DefaultAPIInstance.post(url, data);
+  },
+  editProject(_id, name, code) {
+    const url = '/projects';
+    const data = { _id, name, code };
+    return DefaultAPIInstance.put(url, data);
+  },
+  deleteProject(_id) {
+    const url = `/projects/${_id}`;
+    return DefaultAPIInstance.delete(url, {});
   }
-}
+};
